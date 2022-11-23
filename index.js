90de8ecf6823dde05e4411d4f0073ec09689d78f
spectr-e <devs.josia@gmail.com>
2022-11-23 12:01:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  let firstTwoDrivers = drivers.slice(0, 2);
+  return firstTwoDrivers;
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  let lastTwoDrivers = drivers.slice(-2);
+  return lastTwoDrivers;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  return (fare) => {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = (drivers, selectDrivers) => {
+  {
+    if (selectDrivers === returnFirstTwoDrivers) {
+      return returnFirstTwoDrivers(drivers);
+    } else if (selectDrivers === returnLastTwoDrivers) {
+      return returnLastTwoDrivers(drivers);
+    }
+  }
+};
